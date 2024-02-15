1877f02c5d50a3ca421dd80717482f6f2a8e898d
RajWarudkar <RajWarudkar>
2024-02-15 12:17:41 +0530

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { LeadComponent } from './Components/lead/lead.component';
+import { CityComponent } from './Components/city/city.component'; 
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
   { path: 'login', component: LoginComponent },
   { path: 'lead', component: LeadComponent },
   { path: 'user', loadChildren: () => import('./Components/user/user.module').then(m => m.UserModule) },
-  { path: 'customer', loadChildren: () => import('./Components/customer/customer.component').then(m => m.CustomerComponent) },
- 
+ {path : 'city', component: CityComponent }
 ];
 
 @NgModule({
